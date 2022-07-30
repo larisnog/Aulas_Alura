730c2b6f2088c3d47b0d63e65ebbb99665830895
Larissa Nogueira <larissanogueira033@gmail.com>
2022-07-29 21:12:10 -0300

    conhecendo listas

--- Web/aluramidi-primeiros_passos_js/main.js
+++ Web/aluramidi-primeiros_passos_js/main.js
@@ -1,12 +1,9 @@
+// Pow ------------------------------------------  
 function tocaSomPom() {
     document.querySelector('#som_tecla_pom').play();
 }
-
-function tocaSomClap() {
-    document.querySelector('#som_tecla_clap').play();
-}
-
-
-
 document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick = tocaSomPom;
