e381586b016d4c53b2d5f6d67aa6b1dd9c5a8a5f
Larissa Nogueira <larissanogueira033@gmail.com>
2022-07-29 22:13:02 -0300

    tomando decisões

--- PHP/php-primeiros_passos/strings.php
+++ PHP/php-primeiros_passos/strings.php
@@ -4,3 +4,9 @@ $idade = 21;
 echo "Olá Mundo!" . PHP_EOL;
 /* echo"\t"; tab */ 
 echo "Eu tenho $idade anos";
+
+echo PHP_EOL . PHP_EOL; /*quebra de linha */
+
+echo "Você só pode entrar, se tiver mais de 18 anos\n";
+
+echo "Você tem $idade anos. Pode entrar";
