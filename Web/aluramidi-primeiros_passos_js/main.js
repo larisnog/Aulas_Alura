82b5410df43180031bc8eeddbd4fd1c697112b10
Larissa Nogueira <larissanogueira033@gmail.com>
2022-08-12 21:57:55 -0300

    iterando em listas

--- Web/aluramidi-primeiros_passos_js/main.js
+++ Web/aluramidi-primeiros_passos_js/main.js
@@ -1,9 +1,21 @@
-// Pow ------------------------------------------  
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom;
+for (let contador = 0; contador < listaDeTeclas.length; contador++){
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    const idAudio = `#som_${instrumento}`;
+    
+    tecla.onclick = function( ){
+        tocaSom(idAudio);
+    }
+
+    contador = contador + 1;
+     
+    //console.log(contador);
+}
