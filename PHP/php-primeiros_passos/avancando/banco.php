244a96546276f33892b84aaa92176e3679bb1708
Larissa Nogueira <larissanogueira033@gmail.com>
2022-10-17 12:46:16 -0300

    interpolação e require

--- PHP/php-primeiros_passos/avancando/banco.php
+++ PHP/php-primeiros_passos/avancando/banco.php
@@ -1,30 +1,6 @@
 <?php
 
-function sacar(array $conta, float $valorASacar): array
-{
-    if($valorASacar > $conta['saldo']){
-        exibeMensagem("Você não pode sacar");
-    } else {
-        $conta['saldo'] -= $valorASacar;
-    }
-    return $conta;
-}
-
-function exibeMensagem($mensagem)
-{
-    echo $mensagem . PHP_EOL;
-}
-
-function depositar(array $conta, float $valorADepositar): array
-{
-    if($valorADepositar > 0){
-        $conta['saldo'] += $valorADepositar;
-
-    }else {
-        exibeMensagem("Depósitos precisam ser positivos");
-    }
-    return $conta;
-}
+require_once 'funcoes.php';
 
 $contasCorrentes = [
     '123.456.789-10'  => [
@@ -46,5 +22,5 @@ $contasCorrentes['123.456.789-12'] = sacar($contasCorrentes['123.456.789-12'], 2
 $contasCorrentes['123.456.789-10'] = depositar($contasCorrentes['123.456.789-10'], -300);
  
 foreach($contasCorrentes as $cpf => $conta){
-    exibeMensagem($cpf . " " . $conta['titular'] . " " . $conta['saldo']);
+    exibeMensagem("$cpf {$conta['titular']} {$conta['saldo']}");
 }
---
+++ PHP/php-primeiros_passos/avancando/funcoes.php
@@ -0,0 +1,27 @@
+<?php
+
+function sacar(array $conta, float $valorASacar): array
+{
+    if($valorASacar > $conta['saldo']){
+        exibeMensagem("Você não pode sacar");
+    } else {
+        $conta['saldo'] -= $valorASacar;
+    }
+    return $conta;
+}
+
+function exibeMensagem($mensagem)
+{
+    echo $mensagem . PHP_EOL;
+}
+
+function depositar(array $conta, float $valorADepositar): array
+{
+    if($valorADepositar > 0){
+        $conta['saldo'] += $valorADepositar;
+
+    }else {
+        exibeMensagem("Depósitos precisam ser positivos");
+    }
+    return $conta;
+}
