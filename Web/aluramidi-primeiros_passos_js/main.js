eec4792a7fbf4918a648e0d3527c3440fe6e96d7
Larissa Nogueira <larissanogueira033@gmail.com>
2022-10-20 15:39:02 -0300

    eventos e lógicas

--- Web/aluramidi-primeiros_passos_js/main.js
+++ Web/aluramidi-primeiros_passos_js/main.js
@@ -1,5 +1,12 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+   const elemento = document.querySelector(seletorAudio);
+
+   if(elemento && elemento.localName === 'audio'){
+        elemento.play();
+   }
+   else{
+        console.log('Elemento não encontrado ou seletetor inválido');
+   }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -16,4 +23,14 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++){
     }
      
     //console.log(contador);
+
+    tecla.onkeydown = function(evento){
+        if(evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
 }
