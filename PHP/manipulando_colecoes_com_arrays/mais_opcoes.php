f9b242fafe6245ab257032be4bb96a8239356fae
Larissa Nogueira <larissanogueira033@gmail.com>
2022-11-02 15:03:21 -0300

    fazendo verificações

--- PHP/manipulando_colecoes_com_arrays/mais_opcoes.php
+++ PHP/manipulando_colecoes_com_arrays/mais_opcoes.php
@@ -1,7 +1,7 @@
 <?php
 
 $notas = [
-    'Vinicius' => 6,
+    'Vinicius' => null,
     'Maria' => 9,
     'Ana' => 10,
     'Roberto' => 7,
@@ -12,3 +12,18 @@ $notas = [
 /* rsort($notas); // ordena ordem inversa */
 ksort($notas); // ordena ordem alfabetica 
 var_dump($notas);
+
+if(is_array($notas)){
+    echo 'Sim, é um array' . PHP_EOL;
+}
+
+/* var_dump(array_key_exists('Vinicius', $notas)); //verifica se a chave existe, retorna true */
+
+echo 'Ana fez a prova:' . PHP_EOL;
+var_dump(isset($notas['Ana'])); //verifica se o valor é nulo, retorna false se sim
+
+echo 'Alguem tirou 10?' . PHP_EOL;
+var_dump(in_array(10, $notas, true)); // verifica se existe algum valor
+
+echo 'Quem tirou 10?' . PHP_EOL;
+var_dump(array_search(10, $notas, true)); //busca a chave do valor 
