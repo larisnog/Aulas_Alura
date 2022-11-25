4b665cac0bfeada5daf2c3e00acdd07e8e41d522
Larissa Nogueira <larissanogueira033@gmail.com>
2022-11-25 20:07:36 -0300

    projeto atualizado em andamento controle de séries

---
+++ Laravel 2/controle-series_atualizado/resources/views/series/index.blade.php
@@ -0,0 +1,10 @@
+<x-layout title="Séries"> <!-- componente -->
+    <a href="/series/criar" class="btn btn-dark mb-2">Adicionar</a>
+
+    <ul class="list-group">
+        @foreach ($series as $serie)
+           <li class="list-group-item">{{ $serie->nome }}</li>
+        @endforeach
+    </ul>
+</x-layout>
+
