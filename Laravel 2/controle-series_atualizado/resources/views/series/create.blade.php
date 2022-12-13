6f591fbce737603be7f6b4de278f699f0a13b45a
Larissa Nogueira <larissanogueira033@gmail.com>
2022-12-12 22:46:44 -0300

    upload de imagens implementado

--- Laravel 2/controle-series_atualizado/resources/views/series/create.blade.php
+++ Laravel 2/controle-series_atualizado/resources/views/series/create.blade.php
@@ -1,6 +1,6 @@
 <x-layout title="Nova Série">
     
-    <form action="{{ route('series.store') }}" method="post">
+    <form action="{{ route('series.store') }}" method="post" enctype="multipart/form-data">
         @csrf
 
         <div class="row mb-3">
@@ -20,7 +20,13 @@
                 value="{{ old('episodesPerSeason') }}">
             </div>
         </div>
-        
+        <div class="row mb-3">
+            <div class="col-12">
+                <label for="cover" class="form-label">Capa</label>
+                <input type="file" id="cover" name="cover" class="form-control" accept="image/gif, image/jpeg, image/png">
+            </div>
+        </div>
+
         
         <button type="submit" class="btn btn-primary">Adicionar</button>
     </form>
--- Laravel 2/controle-series_atualizado/resources/views/series/index.blade.php
+++ Laravel 2/controle-series_atualizado/resources/views/series/index.blade.php
@@ -10,9 +10,13 @@
     <ul class="list-group">
         @foreach ($series as $serie)
            <li class="list-group-item d-flex justify-content-between align-items-center">
-                <a href="{{ route('seasons.index', $serie->id) }}">
-                    {{ $serie->nome }}
-                </a>
+                <div class="d-flex align-items-center">
+                    <img class="me-3" src="{{ asset('storage/' . $serie->cover) }}" width="100" class="img-thumbnail" alt="">
+                    <a href="{{ route('seasons.index', $serie->id) }}">
+                        {{ $serie->nome }}
+                    </a>
+                </div>
+                
                 <span class="d-flex">
                     <a href="{{ route('series.edit', $serie->id) }}" class="btn btn-primary btn-sm">
                         E
